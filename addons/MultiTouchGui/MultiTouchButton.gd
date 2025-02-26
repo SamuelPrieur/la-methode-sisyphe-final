0ff07d22a3d975118d1eba716ce471c3cc4550c1
SamuelPrieur <sprieurpro@gmail.com>
2025-02-26 11:10:05 -0500

    renamed:    Assets/MiniJeux/Nitro/bouton on nitro.png -> Assets/MiniJeux/Nitro/nitroOn.png renamed:    Assets/MiniJeux/Nitro/bouton on nitro.png.import -> Assets/MiniJeux/Nitro/nitroOn.png.import modified:   Scenes/Panel1P.tscn modified:   addons/MultiTouchGui/MultiTouchButton.gd modified:   Scenes/Panel2P.tscn

--- addons/MultiTouchGui/MultiTouchButton.gd
+++ addons/MultiTouchGui/MultiTouchButton.gd
@@ -1,25 +1,21 @@
 extends Button
 class_name MultiTouchButton
- 
 @export var icon_normal: Texture
 @export var icon_pressed: Texture
- 
 func _ready():
 	if icon_normal and not icon:
 		icon = icon_normal
- 
 func _gui_input(event):
-	var inside = event.position.x > global_position.x \
-		and event.position.y > global_position.y \
-		and event.position.x < global_position.x + size.x \
-		and event.position.y < global_position.y + size.y
-
-	if event is InputEventScreenTouch and event.pressed :
+	var event_pos_adjusted = event.position + global_position
+	var inside = event_pos_adjusted.x > position.x and event_pos_adjusted.y > position.y and event_pos_adjusted.x < position.x + size.x and event_pos_adjusted.y < position.y + size.y
+	if event is InputEventScreenTouch and event.pressed:
 		if not toggle_mode:
-			toggled.emit()
-			button_pressed = true
+			pressed.emit()
+			button_down.emit()
 			icon = icon_pressed
 
+ 
+ 
 	elif (event is InputEventScreenTouch and inside) or (event is InputEventScreenTouch and !event.pressed and !inside):
 		button_up.emit()
 		icon = icon_normal
