622c8d3446de367ae75abc257de4405267d3dd6d
SamuelPrieur <sprieurpro@gmail.com>
2025-02-26 09:44:12 -0500

    modified:   .DS_Store new file:   "Export/La M\303\251thode Sisyphe.console.exe" new file:   "Export/La M\303\251thode Sisyphe.exe" new file:   "Export/La M\303\251thode Sisyphe.pck" new file:   Export/TEst.zip modified:   Scenes/Minigame/KeyPad.tscn modified:   Scenes/Minigame/Radio.tscn modified:   Scenes/Minigame/eject_button.tscn modified:   Scenes/Minigame/rotation_minigame.tscn modified:   Scenes/Minigame/ventilateur.tscn modified:   Scenes/Panel1P.tscn modified:   Scenes/Panel2P.tscn modified:   Scripts/Minigame/curve_minigame.gd modified:   addons/MultiTouchGui/MultiTouchButton.gd modified:   addons/MultiTouchGui/MultiTouchButtonNob.gd new file:   export_presets.cfg

--- addons/MultiTouchGui/MultiTouchButton.gd
+++ addons/MultiTouchGui/MultiTouchButton.gd
@@ -1,16 +1,27 @@
 extends Button
 class_name MultiTouchButton
+ 
 @export var icon_normal: Texture
 @export var icon_pressed: Texture
+ 
 func _ready():
 	if icon_normal and not icon:
 		icon = icon_normal
-		
+ 
 func _gui_input(event):
-	if event is InputEventScreenTouch and event.pressed:
-		icon = icon_pressed
-		if not toggle_mode:
+	var event_pos_adjusted = event.position + global_position
+	var inside = event_pos_adjusted.x > position.x and event_pos_adjusted.y > position.y and event_pos_adjusted.x < position.x + size.x and event_pos_adjusted.y < position.y + size.y
+	if event is InputEventScreenTouch and event.pressed and inside:
+		if toggle_mode:
 			toggled.emit()
 			button_pressed = true
-	else:
+			icon = icon_pressed
+		else:
+			pressed.emit()
+			button_down.emit()
+
+
+	elif (event is InputEventScreenTouch and inside) or (event is InputEventScreenTouch and !event.pressed and !inside):
+		button_up.emit()
 		icon = icon_normal
+		button_pressed = false
--- addons/MultiTouchGui/MultiTouchButtonNob.gd
+++ addons/MultiTouchGui/MultiTouchButtonNob.gd
@@ -1,10 +1,10 @@
 extends Button
 class_name MultiTouchButtonNob
-
+ 
 @export var icon_position1: Texture
 @export var icon_position2: Texture
 @export var icon_position3: Texture
-
+ 
 var position_icons = []
 var current_icon_index = 0
 
@@ -12,12 +12,19 @@ func _ready():
 	position_icons = [icon_position1, icon_position2, icon_position3]
 	if position_icons[0] != null:
 		icon = position_icons[current_icon_index]
+ 
+func _gui_input(event):
+	var event_pos_adjusted = event.position + global_position
+	var inside = event_pos_adjusted.x > position.x and event_pos_adjusted.y > position.y and event_pos_adjusted.x < position.x + size.x and event_pos_adjusted.y < position.y + size.y
+	if event is InputEventScreenTouch and event.pressed and inside:
+		if toggle_mode:
+			toggled.emit()
+			button_pressed = true
+		else:
+			pressed.emit()
+			button_down.emit()
 
-#func _gui_input(event):
-	#if event is InputEventScreenTouch and event.pressed:
-		#if not toggle_mode:
-			#current_icon_index = (current_icon_index + 1) % position_icons.size()
-			#icon = position_icons[current_icon_index]
-			#print("test")
-			#toggled.emit()
-			#button_pressed = true
+
+	elif (event is InputEventScreenTouch and inside) or (event is InputEventScreenTouch and !event.pressed and !inside):
+		button_up.emit()
+		button_pressed = false
